0aaddd0fb70d3e5da5794ab9a647dc327bcc543d
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2021-04-04 19:29:15 +0430

    Added simple test

---
+++ src/test/java/view/MainTest.java
@@ -0,0 +1,12 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
